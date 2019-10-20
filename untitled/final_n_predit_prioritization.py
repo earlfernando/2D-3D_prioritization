7ea76d38ccbddc1467c922857d06d2d9754a0ecb
earlfernando <earlfernando>
2019-10-20 14:43:59 +0200

    mary college

--- untitled/final_n_predit_prioritization.py
+++ untitled/final_n_predit_prioritization.py
@@ -33,20 +33,20 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
 warnings.filterwarnings("ignore")
 sys.setrecursionlimit(150000000)
 # csv_file_test_image = "/home/earl/Thesis/GreatCourt/test_image.csv"
-database_locatiom = "/home/earlfernando/kingscollege/kingscollege.db"
-image_bin_location = "/home/earlfernando/kingscollege/images.bin"
-csv_file_location_400000 = "/home/earlfernando/kingscollege/training_Data_RandomForest_overall.csv"
-images_test_file_location = "/home/earlfernando/kingscollege/dataset_test.txt"
+database_locatiom = "/home/earlfernando/marycollege/marycollege.db"
+image_bin_location = "/home/earlfernando/marycollege/images.bin"
+csv_file_location_400000 = "/home/earlfernando/marycollege/training_Data_RandomForest_overall.csv"
+images_test_file_location = "/home/earlfernando/marycollege/dataset_test.txt"
 # file_name_random_forest = "/home/earl/Thesis/GreatCourt/test_model_random_forest_10000.sav"
-file_name_kmeans = "/home/earlfernando/kingscollege/test_model_kmeans.sav"
+file_name_kmeans = "/home/earlfernando/marycollege/test_model_kmeans.sav"
 feature_length = 128
-csv_file_location_kmeans = "/home/earlfernando/kingscollege/train_kmeans.csv"
+csv_file_location_kmeans = "/home/earlfernando/marycollege/train_kmeans.csv"
 number_of_clusters = 10000
-database_location_overall = "/home/earlfernando/kingscollege/kingscollege.db"
-image_bin_location_overall = "/home/earlfernando/kingscollege/images.bin"
-point3D_location_overall = "/home/earlfernando/kingscollege/points3D.bin"
+database_location_overall = "/home/earlfernando/marycollege/kingscollege.db"
+image_bin_location_overall = "/home/earlfernando/marycollege/images.bin"
+point3D_location_overall = "/home/earlfernando/marycollege/points3D.bin"
 # csv_file_location_kmeans_test = "/home/earlfernando/greatCourtTrinity/GreatCourt//test_kmeans_modified.csv"
-csv_file_location_kmeans_test = "/home/earlfernando/kingscollege/test_kmeans_modified.csv"
+csv_file_location_kmeans_test = "/home/earlfernando/marycollege/test_kmeans_modified.csv"
 max_cost = 20000
 
 
@@ -337,7 +337,7 @@ def random_forest(headers, feature_length, csv_file_location, file_name):
 
 
 def feature_selection(number):
-    csv_file_location_local = "/home/earl/Thesis/kingscollege/training_Data_RandomForest_10000.csv"
+    csv_file_location_local = "/home/earl/Thesis/marycollege/training_Data_RandomForest_10000.csv"
     if number == 0:
         selected_columns = create_headers(feature_length)
     if number >= 1:
@@ -1517,11 +1517,11 @@ print("kmeans")
 print("random forest saved")
 
 selected_columns = create_headers(feature_length)
-csv_file_location_kmeans = "/home/earlfernando/kingscollege/train_kmeans.csv"
-file_name_kmeans = "/home/earlfernando/kingscollege/test_model_kmeans.sav"
-file_name_random_forest = "/home/earlfernando/kingscollege/dataset_full/noFeature/N=100max_depth=4000min_leaf=10.sav"
+csv_file_location_kmeans = "/home/earlfernando/marycollege/train_kmeans.csv"
+file_name_kmeans = "/home/earlfernando/marycollege/test_model_kmeans.sav"
+file_name_random_forest = "/home/earlfernando/marycollege/dataset_full/noFeature/N=100max_depth=300min_leaf=1.sav"
 
-save_location_picture = "/home/earlfernando/kingscollege/best_plot"
+save_location_picture = "/home/earlfernando/marycollege/best_plot"
 # k_means(headers,feature_length,csv_file_location_kmeans,file_name_kmeans)
 # k_means_broken_samples(headers,feature_length,csv_file_location_kmeans,file_name_kmeans,number_of_clusters)
 
